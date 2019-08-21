b79ab33f133992dbe9da081a9b1ca70ba3fcaaf8
Marco Guidolin <m.guidolin@egeatech.com>
2019-08-21 20:05:50 +0200

    WIP: about page scaffolding

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container
+    pt-0
+    fluid
+  >
+    <v-row
+      class="welcome-slide full-viewport"
+    >
+      <v-col class="about">
+        <h1>This is an about page</h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
