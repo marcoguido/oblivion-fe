a602521e8952cd61397f83f7289e657c3271b04c
Marco Guidolin <m.guidolin@egeatech.com>
2019-08-23 19:05:53 +0200

    ADD: vuex persistent storage

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -38,4 +39,5 @@ export default new Vuex
         }
       },
     },
+    plugins: [createPersistedState()],
   });
