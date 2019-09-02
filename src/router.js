5a2f88bacc644d139eb012a272e14642d97062ad
Marco Guidolin <m.guidolin@egeatech.com>
2019-09-02 21:39:34 +0200

    TEST: routing issue

--- src/router.js
+++ src/router.js
@@ -29,7 +29,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "privacy-policy" */ './views/NotFound.vue'),
     },
   ],
-  scrollBehavior() {
-    return { x: 0, y: 0 };
-  },
+  // scrollBehavior() {
+  //   return { x: 0, y: 0 };
+  // },
 });
